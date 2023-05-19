680c0debae34da7b7e7dccfaa455cde848a4427b
zurimokato <noj2304@gmail.com>
2023-05-19 12:37:00 -0500

    add fetch manga api logic adn ui

--- src/components/MangaAPi.jsx
+++ src/components/MangaAPi.jsx
@@ -1,18 +1,41 @@
 import React from 'react'
 
-function MangaAPi() {
-    const obtenerManga=async()=>{
+function MangaApi() {
+     const [mangas,setMangas]=React.useState([]);
+     const [paginacion, setPaginacion]=React.useState(1);
+     const obtenerManga=async()=>{
         console.log("buscando manga!!");
+        const requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+          };
+
+          
+          try {
+            const response = await fetch(`https://api.jikan.moe/v4/manga?page=${paginacion}&orderby=mal_id`, requestOptions);
+            const result = await response.text();
+            const data= JSON.parse(result).data;
+
+            setMangas(data)
+            console.log(paginacion);
+
+
+            console.log(data);
+          } catch (error) {
+            console.log('error', error);
+          }
+
+       
 
     }
     const adelante=()=>{
-        console.log("paginando hacia adelante!!");
-
+        setPaginacion(paginacion+1);
+        obtenerManga();
     }
-    const atras=()=>{
-        console.log("buscando paginando hacia atras!!");
 
-        
+    const atras=()=>{
+        setPaginacion(paginacion-1);
+        obtenerManga();
     }
   return (
     <div>
@@ -20,8 +43,22 @@ function MangaAPi() {
         <button onClick={obtenerManga}>Traer Mangas</button>
         <button onClick={adelante}>Siguiente</button>
         <button onClick={atras}>Atrás</button>
+        {
+            mangas.map((manga)=>(
+                <div key={manga.mal_id}>
+                    <h3>{manga.mal_id} -  {manga.title}</h3>
+                    <h4>Popularidad:  {manga.popularity}</h4>
+                    <h4>Número de volumenes:  {manga.volumes}</h4>
+                    <img src={manga.images.jpg.large_image_url} alt={manga.title} />
+                    <hr></hr>
+                </div>
+                
+            ))
+        
+        }
+
     </div>
   )
 }
 
-export default MangaAPi
+export default MangaApi
