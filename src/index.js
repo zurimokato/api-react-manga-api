245e2545b641e02b32335231fdb638750ac0fe91
zurimokato <noj2304@gmail.com>
2023-05-19 12:41:10 -0500

    main

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import MangaApi from './components/MangaApi';
+import MangaApi from './components/MangaAPi';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
