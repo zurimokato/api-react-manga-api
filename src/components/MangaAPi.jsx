17d234609a807a2301d17a5ae44313630b13bc16
zurimokato <noj2304@gmail.com>
2023-05-19 11:31:25 -0500

    adding skeleton

--- src/components/MangaAPi.jsx
+++ src/components/MangaAPi.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
 
 function MangaAPi() {
+    const obtenerManga=async()=>{
+        console.log("buscando manga!!");
+
+    }
+    const adelante=()=>{
+        console.log("paginando hacia adelante!!");
+
+    }
+    const atras=()=>{
+        console.log("buscando paginando hacia atras!!");
+
+        
+    }
   return (
-    <div>MangaAPi</div>
+    <div>
+        <h1>Petición al api de rick and morty</h1>
+        <button onClick={obtenerManga}>Traer Personaje</button>
+        <button onClick={adelante}>Siguiente</button>
+        <button onClick={atras}>Atrás</button>
+    </div>
   )
 }
 
